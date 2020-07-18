4639c091fa7ef86ac4904bf4fe9026e64de79896
Ariane T <arianet@Arianes-MBP.localdomain>
2020-07-18 16:49:01 -0400

    java.city devoir sur les variables

---
+++ city.java
@@ -0,0 +1,15 @@
+public class city{
+
+    public static int  houseNumber = 2;
+    String houseState= "virginia";
+  
+      public static void main(String[] args){
+        char houseGroup = 'B';
+        city obj = new city ();
+        System.out.println( "house state  " + obj.houseState);
+        System.out.println("house group   " + houseGroup);
+        System.out.println("house number   " + houseNumber);
+        
+  
+      }//End of main
+    }
